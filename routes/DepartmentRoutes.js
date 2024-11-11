42ac7e265f9aca0ee23d1dcd733399a074818c43
Elijah Nicholas Esguerra <ajai.esg17@gmail.com>
2024-11-11 22:33:11 +0800

    fixed retrieval of available departments

--- routes/DepartmentRoutes.js
+++ routes/DepartmentRoutes.js
@@ -1,7 +1,8 @@
 // routes/departmentRoutes.js
 import express from "express";
+import { getDepartmentsDataController } from "../controllers/DepartmentController.js";
 const departmentRoutes = express.Router();
 
-departmentRoutes.get("/list");
+departmentRoutes.get("/list", getDepartmentsDataController);
 
 export default departmentRoutes;
